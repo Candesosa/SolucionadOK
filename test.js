3a6a87b0f08d031724d8b050b8ca6667b8e3533b
candesosa <mcandelariasosa@gmail.com>
2021-06-02 23:06:41 -0300

    errorJSboton

--- test.js
+++ test.js
@@ -1,3 +1,13 @@
+addEventListener("DOMContentLoaded", () => {
+    const btn_menu = document.querySelector(".btn_menu")
+    if (btn_menu) {
+        btn_menu.addEventListener("click", () => {
+            const menu_items = document.querySelector(".menu_items")
+            menu_items.classList.toggle("show")
+        })
+    }
+})
+
 document.querySelector('.img__btn').addEventListener('click', function() {
-    document.querySelector('.cont').classList.toggle('s--signup');
-  });
+  document.querySelector('.cont').classList.toggle('s--signup');
+});
