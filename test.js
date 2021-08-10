aedbb045ddcdd06b558e34427368f9f7c8f3afa3
candesosa <mcandelariasosa@gmail.com>
2021-08-10 16:32:56 -0300

    Login-Logout

--- test.js
+++ test.js
@@ -1,13 +1,9 @@
-addEventListener("DOMContentLoaded", () => {
-    const btn_menu = document.querySelector(".btn_menu")
+addEventListener('DOMContentLoaded', () => {
+    const btn_menu = document.querySelector('.btn_menu')
     if (btn_menu) {
-        btn_menu.addEventListener("click", () => {
-            const menu_items = document.querySelector(".menu_items")
-            menu_items.classList.toggle("show")
+        btn_menu.addEventListener('click', () => {
+            const menu_items = document.querySelector('.menu_items')
+            menu_items.classList.toggle('show')
         })
     }
 })
-
-document.querySelector('.img__btn').addEventListener('click', function() {
-  document.querySelector('.cont').classList.toggle('s--signup');
-});
